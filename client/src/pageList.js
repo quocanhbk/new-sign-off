32537177ece66e92be3d3d22789a874b432de174
La Quoc Anh <anh.lq@trungthuy.vn>
2021-05-10 14:37:56 +0700

    update

--- client/src/pageList.js
+++ client/src/pageList.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {BsHouse, BsSearch, BsFileEarmarkPlus, BsPencilSquare, BsFileEarmarkCheck, BsFileEarmarkMinus} from 'react-icons/bs'
+import {BsHouse, BsSearch, BsFileEarmarkPlus, BsPencilSquare, BsFileEarmarkCheck, BsFileEarmarkMinus, BsGear} from 'react-icons/bs'
 import HomePage from './components/Home'
 import CreatePage from './components/Create'
 import DeletedPage from './components/Deleted'
 import SearchPage from './components/Search'
 import SignPage from './components/Sign'
 import CompletedPage from './components/Completed'
-
+import PlaygroundPage from './components/Playground'
 
 const pageList = [
     {icon: <BsHouse size="24px"/>, text: "Home", path: "/", comp: <HomePage/>},
@@ -15,6 +15,7 @@ const pageList = [
     {icon: <BsPencilSquare size="24px"/>, text: "Sign Document", path: "/sign", comp: <SignPage/>},
     {icon: <BsFileEarmarkCheck size="24px"/>, text: "Completed Document", path: "/completed", comp: <CompletedPage/>},
     {icon: <BsFileEarmarkMinus size="24px"/>, text: "Deleted Document", path: "/deleted", comp: <DeletedPage/>},
+    {icon: <BsGear size="24px"/>, text: "Playground", path: "/playground", comp: <PlaygroundPage/>},
 ]
 
 export default pageList
