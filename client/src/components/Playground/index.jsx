b2c74f54144ff405763341116003fd6ab888ae35
La Quoc Anh <anh.lq@trungthuy.vn>
2021-05-12 07:39:32 +0700

    feat: backend structure

--- client/src/components/Playground/index.jsx
+++ client/src/components/Playground/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import ImageItem from './ImageItem'
@@ -17,38 +17,76 @@ const ImgContainer = styled.div`
     }
 `
 const Playground = () => {
-    const [image, setImage] = useState()
     const [imgArray, setImgArray] = useState([])
     const [isAdd, setIsAdd] = useState(false)
-    const onFormSubmit = (e) => {
+    const [tagTitle, setTagTitle] = useState("")
+    const [selectedTag, setSelectedTag] = useState()
+
+    const handleChange = (e) => {
         e.preventDefault()
+        if (!e.target.files[0]) return
         const formData = new FormData();
-        formData.append('myImage', image)
+        formData.append('myImage', e.target.files[0])
         const config = {
             headers: {
                 'content-type': 'multipart/form-data'
             }
         }
-        axios.post(process.env.PORT || 'http://localhost:5000/upload', formData, config).then((res) => setImgArray(res.data.path))
+        axios.post('/upload', formData, config).then((res) => {
+            let count = 1
+            setImgArray(res.data.path.map(p => ({id: count++, path: p, tagList: []})))
+        })
     }
+    const addTextTag = (path, pos) => {
+        if (isAdd) {
+            let item = imgArray.find(item => item.path === path)
+            item.tagList.push({name: tagTitle, position: pos, content: ""})
+            setImgArray([...imgArray.filter(item => item.path !== path), item].sort((a, b) => a.id - b.id))
+            setIsAdd(false)
+            setTagTitle("")
+        }
+    }
+    const selectTag = (path, name) => {
+        setSelectedTag({path, name})
+    }
+    const changeTagContent = (e) => {
+        let currentItem = imgArray.find(item => item.path === selectedTag.path)
+        let currentTag = currentItem.tagList.find(tag => tag.name === selectedTag.name)
+        currentTag.content = e.target.value
 
-    const handleChange = (e) => {
-        setImage(e.target.files[0])
+        setImgArray([...imgArray.filter(item => item.path !== selectedTag.path), currentItem].sort((a, b) => a.id - b.id))
+    }
+    const getContent = () => {
+        if (!selectedTag)
+            return ""
+        return imgArray.find(item => item.path === selectedTag.path).tagList.find(tag => tag.name === selectedTag.name).content
     }
     return (
         <Container>
             <div>
-                <form onSubmit={onFormSubmit}>
+                <form>
                     <h1>File Upload</h1>
-                    <input type="file" name="myImage" onChange={handleChange}/>
-                    <button type="submit">Upload</button>
+                    <input type="file" name="myImage" onChange={handleChange} accept=".pdf"/>
                 </form>
-                <button onClick={() => setIsAdd(true)}>CLick me</button>
+                <form>
+                    <label>Click Add Text Tag then hover the document</label>
+                    <br/>
+                    <input type="text" value={tagTitle} onChange={(e) => setTagTitle(e.target.value)}/>
+                    <button onClick={(e) => {e.preventDefault();setIsAdd(true)}}>Add Text Tag</button>
+                </form>
+                {selectedTag && <input type="text" value={getContent()} onChange={changeTagContent}/>}
             </div>
             
             <ImgContainer>
-                {imgArray.map(path => 
-                    <ImageItem key={path} src={path} isAdd={isAdd} setIsAdd={setIsAdd} />    
+                {imgArray.map(item => 
+                    <ImageItem 
+                        key={item.path} 
+                        src={item.path} 
+                        isAdd={isAdd} 
+                        addTextTag={(pos) => addTextTag(item.path, pos)} 
+                        tagList={item.tagList}
+                        selectTag={(name) => selectTag(item.path, name)}
+                    />    
                 )}
             </ImgContainer>
         </Container>
