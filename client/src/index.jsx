3fd48355859c75b0a869e066e801ff7127bdf158
KS <sonngo81.99gmail.com>
2021-06-21 17:27:32 +0700

    feat(authentication): configured

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as msal from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import './index.css';
+import App from './App';
+import config from 'msalConfig';
+
+const msalInstance = new msal.PublicClientApplication(config);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ client/src/msalConfig.js
@@ -0,0 +1,9 @@
+
+const config = {
+  auth: {
+    clientId: 'f8626081-3f81-4909-bcef-14f0a974f079',
+    authority: 'https://login.microsoftonline.com/801edb4c-29ba-4d07-80b1-16a955dbf20d',
+  }
+};
+
+export default config;
