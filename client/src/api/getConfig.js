94c9850febaec697cccec02d719f65c1c799c168
KS <sonngo81.99gmail.com>
2021-07-23 10:46:47 +0700

    fix(token-refresh):
    - Catch InteractionRequiredAuthError and require re-login
    - Add redirect url to authentication config

--- client/src/api/getConfig.js
+++ client/src/api/getConfig.js
@@ -1,21 +1,31 @@
-import baseURL from './baseURL'
+import baseURL from './baseURL';
 import { msalInstance } from '../index';
+import { InteractionRequiredAuthError } from '@azure/msal-browser';
 
+
+let request;
 const getConfig = async () => {
+  try {
     const accounts = msalInstance.getAllAccounts();
-	const request = {
-		scopes: ['api://f8626081-3f81-4909-bcef-14f0a974f079/search_list'],
-		account: accounts[0],
-	}
-	const token = await msalInstance.acquireTokenSilent(request);
+    request = {
+      scopes: ['api://f8626081-3f81-4909-bcef-14f0a974f079/search_list'],
+      account: accounts[0],
+    };
+    const token = await msalInstance.acquireTokenSilent(request);
+    const config = {
+      baseURL,
+      headers: {
+        Authorization: `Bearer ${token.accessToken}`,
+      },
+    };
+    return config;
+  } catch (err) {
+    if (err instanceof InteractionRequiredAuthError) {
+      await msalInstance.acquireTokenPopup(request);
+    } else {
+      throw new Error('Press F for token refresh');
+    }
+  }
+};
 
-	const config = {
-		baseURL,
-		headers: {
-			Authorization: `Bearer ${token.accessToken}`,
-		}
-	}
-    return config
-}
-
-export default getConfig
+export default getConfig;
