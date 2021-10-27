9da6e85fb0d521226119f5938b9913d10169b55c
KS <sonngo81.99@gmail.com>
2021-10-27 15:02:00 +0700

    feat(attachments): add option to hide add button fix(approval-info): document ID grammar

--- client/src/components/Base/AttachmentChecklist.tsx
+++ client/src/components/Base/AttachmentChecklist.tsx
@@ -1,81 +1,98 @@
-import { Fragment } from "react"
-import { BsCheckCircle, BsPlus } from "react-icons/bs"
-import AttachmentTablePC, { AttachmentTableProps } from "./AttachmentTablePC"
-import { IRequestInput } from "api/request"
-import { Id } from "types"
-import { Box, Button, chakra, Flex, IconButton, Text } from "@chakra-ui/react"
+import { Fragment } from 'react';
+import { BsCheckCircle, BsPlus } from 'react-icons/bs';
+import AttachmentTablePC, { AttachmentTableProps } from './AttachmentTablePC';
+import { IRequestInput } from 'api/request';
+import { Id } from 'types';
+import { Box, Button, chakra, Flex, IconButton, Text } from '@chakra-ui/react';
 
-interface AttachmentChecklistProps extends Omit<AttachmentTableProps, "noHeader"> {
-    checklist: IRequestInput["checklist"]
-    setAddingAttachment?: (id: Id) => void
-    readOnly?: boolean
+interface AttachmentChecklistProps
+  extends Omit<AttachmentTableProps, 'noHeader'> {
+  checklist: IRequestInput['checklist'];
+  setAddingAttachment?: (id: Id) => void;
+  readOnly?: boolean;
+  hideAddButton?: boolean;
 }
 
 const AttachmentChecklist = ({
-    checklist,
-    attachments,
-    onRemoveAttachment,
-    onEditAttachment,
-    changeFieldContent,
-    setAddingAttachment,
-    readOnly,
-    requestId,
+  checklist,
+  attachments,
+  onRemoveAttachment,
+  onEditAttachment,
+  changeFieldContent,
+  setAddingAttachment,
+  readOnly,
+  requestId,
+  hideAddButton,
 }: AttachmentChecklistProps) => {
-    return (
-        <Box rounded="md" overflow="hidden" border="1px" borderColor="gray.200">
-            <chakra.table w="full" xs={{ "table-collapse": "collapse" }}>
-                <chakra.tbody>
-                    {checklist.map(checkItem => (
-                        <Fragment key={checkItem.id}>
-                            <chakra.tr bg="gray.100">
-                                <chakra.td colSpan={2}>
-                                    <Flex align="center">
-                                        <IconButton
-                                            color={
-                                                attachments.filter(a => a.checklistItemId === checkItem.id).length > 0
-                                                    ? "green.400"
-                                                    : "gray.400"
-                                            }
-                                            icon={<BsCheckCircle size="1.4rem" />}
-                                            aria-label="checklist"
-                                            variant="ghost"
-                                            pointerEvents="none"
-                                        />
-                                        <Text flex={1}>{checkItem.name}</Text>
-                                    </Flex>
-                                </chakra.td>
-                                <chakra.td w="10%" px={2}>
-                                    <Button
-                                        leftIcon={<BsPlus />}
-                                        size="xs"
-                                        variant="ghost"
-                                        onClick={() => setAddingAttachment && setAddingAttachment(checkItem.id)}
-                                    >
-                                        Attachment
-                                    </Button>
-                                </chakra.td>
-                            </chakra.tr>
-                            {attachments.filter(a => a.checklistItemId === checkItem.id).length > 0 && (
-                                <chakra.tr>
-                                    <chakra.td colSpan={3} borderTop="1px" borderBottom="1px" borderColor="gray.200">
-                                        <AttachmentTablePC
-                                            attachments={attachments.filter(a => a.checklistItemId === checkItem.id)}
-                                            onRemoveAttachment={onRemoveAttachment}
-                                            onEditAttachment={onEditAttachment}
-                                            changeFieldContent={changeFieldContent}
-                                            noHeader
-                                            readOnly={readOnly}
-                                            requestId={requestId}
-                                        />
-                                    </chakra.td>
-                                </chakra.tr>
-                            )}
-                        </Fragment>
-                    ))}
-                </chakra.tbody>
-            </chakra.table>
-        </Box>
-    )
-}
+  return (
+    <Box rounded="md" overflow="hidden" border="1px" borderColor="gray.200">
+      <chakra.table w="full" xs={{ 'table-collapse': 'collapse' }}>
+        <chakra.tbody>
+          {checklist.map((checkItem) => (
+            <Fragment key={checkItem.id}>
+              <chakra.tr bg="gray.100">
+                <chakra.td colSpan={2}>
+                  <Flex align="center">
+                    <IconButton
+                      color={
+                        attachments.filter(
+                          (a) => a.checklistItemId === checkItem.id
+                        ).length > 0
+                          ? 'green.400'
+                          : 'gray.400'
+                      }
+                      icon={<BsCheckCircle size="1.4rem" />}
+                      aria-label="checklist"
+                      variant="ghost"
+                      pointerEvents="none"
+                    />
+                    <Text flex={1}>{checkItem.name}</Text>
+                  </Flex>
+                </chakra.td>
+                <chakra.td w="10%" px={2}>
+                  {!hideAddButton && (
+                    <Button
+                      leftIcon={<BsPlus />}
+                      size="xs"
+                      variant="ghost"
+                      onClick={() =>
+                        setAddingAttachment && setAddingAttachment(checkItem.id)
+                      }
+                    >
+                      Attachment
+                    </Button>
+                  )}
+                </chakra.td>
+              </chakra.tr>
+              {attachments.filter((a) => a.checklistItemId === checkItem.id)
+                .length > 0 && (
+                <chakra.tr>
+                  <chakra.td
+                    colSpan={3}
+                    borderTop="1px"
+                    borderBottom="1px"
+                    borderColor="gray.200"
+                  >
+                    <AttachmentTablePC
+                      attachments={attachments.filter(
+                        (a) => a.checklistItemId === checkItem.id
+                      )}
+                      onRemoveAttachment={onRemoveAttachment}
+                      onEditAttachment={onEditAttachment}
+                      changeFieldContent={changeFieldContent}
+                      noHeader
+                      readOnly={readOnly}
+                      requestId={requestId}
+                    />
+                  </chakra.td>
+                </chakra.tr>
+              )}
+            </Fragment>
+          ))}
+        </chakra.tbody>
+      </chakra.table>
+    </Box>
+  );
+};
 
-export default AttachmentChecklist
+export default AttachmentChecklist;
--- client/src/components/Search/Detail/Content.tsx
+++ client/src/components/Search/Detail/Content.tsx
@@ -1,88 +1,96 @@
-import EventComments from "./EventComments"
-import { projectList } from "constant"
-import { AttachmentChecklist, DescriptionEditor, SectionContainer } from "components/Base"
-import { Flex, Text, Wrap } from "@chakra-ui/react"
-import { ProjectTag } from "components/Base/Tags"
-import AttachmentTablePC from "components/Base/AttachmentTablePC"
-import ViewAttachmentModal from "./ViewAttachmentModal"
-import { useRequestContext } from "./RequestProvider"
+import EventComments from './EventComments';
+import { projectList } from 'constant';
+import {
+  AttachmentChecklist,
+  DescriptionEditor,
+  SectionContainer,
+} from 'components/Base';
+import { Flex, Text, Wrap } from '@chakra-ui/react';
+import { ProjectTag } from 'components/Base/Tags';
+import AttachmentTablePC from 'components/Base/AttachmentTablePC';
+import ViewAttachmentModal from './ViewAttachmentModal';
+import { useRequestContext } from './RequestProvider';
 
 const Content = () => {
-    const { request: req, setViewingAttachment } = useRequestContext()
-    const request = req!
-    return (
-        <Flex h="full" w="full" justify="center">
-            <ViewAttachmentModal />
-            <Flex direction="column" h="full" w="full" maxW="48rem">
-                <SectionContainer title="Related Project">
-                    <Wrap>
-                        {request.relatedProjects.map(project => (
-                            <ProjectTag key={project} project={projectList.find(p => p.id === project)!.text} />
-                        ))}
-                    </Wrap>
-                </SectionContainer>
-                <SectionContainer title="Description">
-                    {request.description ? (
-                        // <div dangerouslySetInnerHTML={{__html: request.description}}></div> :
-                        <DescriptionEditor description={request.description} readOnly />
-                    ) : (
-                        <Text as="i">No description</Text>
-                    )}
-                </SectionContainer>
-                <SectionContainer title="Approval File">
-                    {request.approvalAttachments.length > 0 ? (
-                        request.type === "Procedure" ? (
-                            <AttachmentChecklist
-                                requestId={request.id}
-                                attachments={request.approvalAttachments}
-                                checklist={request.checklist}
-                                readOnly={true}
-                                onEditAttachment={attachmentId =>
-                                    setViewingAttachment({
-                                        type: "approvalAttachments",
-                                        id: attachmentId,
-                                    })
-                                }
-                            />
-                        ) : (
-                            <AttachmentTablePC
-                                requestId={request.id}
-                                attachments={request.approvalAttachments}
-                                readOnly={true}
-                                onEditAttachment={attachmentId =>
-                                    setViewingAttachment({
-                                        type: "approvalAttachments",
-                                        id: attachmentId,
-                                    })
-                                }
-                            />
-                        )
-                    ) : (
-                        <Text as="i">No file</Text>
-                    )}
-                </SectionContainer>
-                <SectionContainer title="Reference File">
-                    {request.referenceAttachments.length > 0 ? (
-                        <AttachmentTablePC
-                            attachments={request.referenceAttachments}
-                            readOnly={true}
-                            onEditAttachment={attachmentId =>
-                                setViewingAttachment({
-                                    type: "referenceAttachments",
-                                    id: attachmentId,
-                                })
-                            }
-                        />
-                    ) : (
-                        <Text as="i">No file</Text>
-                    )}
-                </SectionContainer>
-                <SectionContainer title={"Event & Comments"}>
-                    <EventComments />
-                </SectionContainer>
-            </Flex>
-        </Flex>
-    )
-}
+  const { request: req, setViewingAttachment } = useRequestContext();
+  const request = req!;
+  return (
+    <Flex h="full" w="full" justify="center">
+      <ViewAttachmentModal />
+      <Flex direction="column" h="full" w="full" maxW="48rem">
+        <SectionContainer title="Related Project">
+          <Wrap>
+            {request.relatedProjects.map((project) => (
+              <ProjectTag
+                key={project}
+                project={projectList.find((p) => p.id === project)!.text}
+              />
+            ))}
+          </Wrap>
+        </SectionContainer>
+        <SectionContainer title="Description">
+          {request.description ? (
+            // <div dangerouslySetInnerHTML={{__html: request.description}}></div> :
+            <DescriptionEditor description={request.description} readOnly />
+          ) : (
+            <Text as="i">No description</Text>
+          )}
+        </SectionContainer>
+        <SectionContainer title="Approval File">
+          {request.approvalAttachments.length > 0 ? (
+            request.type === 'Procedure' ? (
+              <AttachmentChecklist
+                hideAddButton={true}
+                requestId={request.id}
+                attachments={request.approvalAttachments}
+                checklist={request.checklist}
+                readOnly={true}
+                onEditAttachment={(attachmentId) =>
+                  setViewingAttachment({
+                    type: 'approvalAttachments',
+                    id: attachmentId,
+                  })
+                }
+              />
+            ) : (
+              <AttachmentTablePC
+                requestId={request.id}
+                attachments={request.approvalAttachments}
+                readOnly={true}
+                onEditAttachment={(attachmentId) =>
+                  setViewingAttachment({
+                    type: 'approvalAttachments',
+                    id: attachmentId,
+                  })
+                }
+              />
+            )
+          ) : (
+            <Text as="i">No file</Text>
+          )}
+        </SectionContainer>
+        <SectionContainer title="Reference File">
+          {request.referenceAttachments.length > 0 ? (
+            <AttachmentTablePC
+              attachments={request.referenceAttachments}
+              readOnly={true}
+              onEditAttachment={(attachmentId) =>
+                setViewingAttachment({
+                  type: 'referenceAttachments',
+                  id: attachmentId,
+                })
+              }
+            />
+          ) : (
+            <Text as="i">No file</Text>
+          )}
+        </SectionContainer>
+        <SectionContainer title={'Event & Comments'}>
+          <EventComments />
+        </SectionContainer>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Content
+export default Content;
