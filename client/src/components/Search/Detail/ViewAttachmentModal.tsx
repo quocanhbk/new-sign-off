e2d80f67ac0997d36eee8684f38aaa77fbeb4541
KS <sonngo81.99@gmail.com>
2021-10-28 13:57:59 +0700

    feat(attachment-modal): - change condition on showing stamp - change error message

--- client/src/components/Search/Detail/ViewAttachmentModal.tsx
+++ client/src/components/Search/Detail/ViewAttachmentModal.tsx
@@ -1,23 +1,35 @@
 // * DESCRIPTION: view attachment on search page
 
-import { ChakraModal } from "components/Base"
-import FormPopup from "./FormPopup"
-import { useRequestContext } from "./RequestProvider"
+import { ChakraModal } from 'components/Base';
+import FormPopup from './FormPopup';
+import { useRequestContext } from './RequestProvider';
 
 const ViewAttachmentModal = () => {
-    const { viewingAttachment, request, setViewingAttachment } = useRequestContext()
-    const req = request!
-    return (
-        <ChakraModal isOpen={!!viewingAttachment} onClose={() => setViewingAttachment(null)} size="6xl" noPadding>
-            {viewingAttachment && (
-                <FormPopup
-                    attachment={req[viewingAttachment.type].find(attachment => attachment.id === viewingAttachment.id)}
-                    requestId={req.id}
-                    isApproved={req.status === "Approved"}
-                />
-            )}
-        </ChakraModal>
-    )
-}
+  const {
+    viewingAttachment,
+    request,
+    setViewingAttachment,
+  } = useRequestContext();
+  const req = request!;
+  return (
+    <ChakraModal
+      isOpen={!!viewingAttachment}
+      onClose={() => setViewingAttachment(null)}
+      size="6xl"
+      noPadding
+    >
+      {viewingAttachment && (
+        <FormPopup
+          attachment={req[viewingAttachment.type].find(
+            (attachment) => attachment.id === viewingAttachment.id
+          )}
+          requestId={req.id}
+          showStampButton={viewingAttachment.type === 'approvalAttachments'}
+          isApproved={req.status === 'Approved'}
+        />
+      )}
+    </ChakraModal>
+  );
+};
 
-export default ViewAttachmentModal
+export default ViewAttachmentModal;
--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,7 +1,11 @@
-export const removeUndefinedProps = obj => {
-    const newObj = { ...obj }
-    Object.keys(newObj).forEach(key => {
-        obj[key] === undefined && delete obj[key]
-    })
-    return newObj
-}
+export const removeUndefinedProps = (obj) => {
+  const newObj = { ...obj };
+  Object.keys(newObj).forEach((key) => {
+    obj[key] === undefined && delete obj[key];
+  });
+  return newObj;
+};
+
+export const extractFileExtension = (filename: string) => {
+  return filename.split('.').pop();
+};
